47dcdae832990e15604a0a45eaae41bfa30b53c9
Vaibhav <sukalevaibhav53@gmail.com>
2024-08-25 19:16:14 +0530

    Youtube Project

---
+++ src/constant/youtube.js
@@ -0,0 +1,5 @@
+export const API_KEY = "AIzaSyD_yYOGi3ZCmzIceK5P7_EMzOkq4VHB2G8";
+export const BASE_URL = "https://www.googleapis.com/youtube/v3"
+export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
+export const SEARCH_SUGGESTIONS_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export default API_KEY;
